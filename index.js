c11d0ac20f4fe8f6020b360c44d314d7e1dc9c01
John Solomou <solomoufam@yahoo.com>
2022-07-10 08:46:08 -0400

    email will send to my gmail now

--- index.js
+++ index.js
@@ -1,33 +1,35 @@
 const express = require("express");
-const app = express();
+
 require("dotenv").config();
 const port = process.env.PORT || 4000;
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const nodemailer = require("nodemailer");
-
+const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(cors());
-
-app.post("/send_mail", cors(), async (req, res) => {
+app.get("/", async (req, res) => {
+  res.json({ message: "testing " });
+});
+app.post("/send_mail", async (req, res) => {
   let { name, email, subject, message } = req.body;
   const transport = nodemailer.createTransport({
-    host: process.env.MAIL_HOST,
-    port: process.env.MAIL_PORT,
+    service: "gmail",
     auth: {
-      user: process.env.MAIL_USER,
-      pass: process.env.MAIL_PASS,
+      user: "jsolomou79@gmail.com",
+      pass: process.env.PASSWORD,
     },
   });
 
-  await transport.sendMail({
-    from: `${email}`,
-    // process.env.MAIL_FROM,
-    to: "solomoufam@yahoo.com",
-    subject: "email from your portfolio",
-    html: `<div className="email">
+  await transport.sendMail(
+    {
+      from: email,
+      // process.env.MAIL_FROM,
+      to: "jsolomou79@gmail.com",
+      subject: "email from your portfolio",
+      html: `<div className="email">
     <h2>Portfolio email! From:${name}</h2>
 
     <p>Email:${email}</p>
@@ -37,7 +39,17 @@ app.post("/send_mail", cors(), async (req, res) => {
     <p>All the best,${name}</p>
      </div>
 `,
-  });
+    },
+    (err) => {
+      if (err) {
+        console.log("there is an error", err);
+      } else {
+        console.log("email has sent!");
+      }
+    }
+  );
+
+  res.json({ message: "testing " });
 });
 
 app.listen(port, () => {
